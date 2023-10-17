4729ecec36f3b66840f96c64a9db2815f48ad3ef
jaegonlee <jaegonlee0@gmail.com>
2023-10-17 14:30:29 +0900

    Update particle.js

--- ai_hitchhiker/particle.js
+++ ai_hitchhiker/particle.js
@@ -1,10 +1,11 @@
 class Particle {
   constructor(x, y, d) {
     this.x = x + random(-파티클설정.생성위치랜덤, 파티클설정.생성위치랜덤);
-    this.y = y + random(-파티클설정.생성위치랜덤, 파티클설정.생성위치랜덤);               this.ax = 0;
+    this.y = y + random(-파티클설정.생성위치랜덤, 파티클설정.생성위치랜덤);    
+    this.ax = 0;
     this.ay = 0;
     this.gravity = 파티클설정.중력;
-    this.turbulence = 파티클설정.난기류;    
+    this.turbulence = 파티클설정.난기류;
     this.vx = random(-1, 1) * 3;
     this.vy = random(-1, 1) * 3;
     this.angle = 0;
@@ -17,8 +18,8 @@ class Particle {
     if (this.turbulence > 0) {
       this.angle =
         noise(this.x * 0.01, this.y * 0.01, frameCount * 0.03) * TWO_PI * 2;
-      this.ax = Math.cos(this.angle) * 0.5;
-      this.ay = Math.sin(this.angle) * 0.5;
+      this.ax = cos(this.angle) * 0.5;
+      this.ay = sin(this.angle) * 0.5;
     }
     this.ay += this.gravity;
     this.vx += this.ax;
@@ -31,10 +32,12 @@ class Particle {
   }
 
   draw() {
+    // fill(255, this.life);
     this.shape(this.x, this.y, this.life);
     this.life = this.life - this.decay;
   }
 }
+
 
 class Trail {
   constructor(x, y, d) {
@@ -43,7 +46,10 @@ class Trail {
 
     this.x[0] = x;
     this.y[0] = y;
-    this.shape = d;
+    this.shape = function () {
+      strokeWeight(1);
+      stroke(255);      
+    };
     this.capacity = 100;
   }
 
@@ -81,8 +87,9 @@ class Trail {
 
   draw() {
     noFill();
-    strokeWeight(4);
-    stroke(255);
+    // strokeWeight(4);
+    // stroke(255);
+    this.shape();
     beginShape();
     for (let i = 0; i < this.x.length; i++) {
       vertex(this.x[i], this.y[i]);
@@ -145,11 +152,13 @@ class Body {
 }
 
 class Avatar {
-  constructor() {
+  constructor(joints) {
+    this.joints = joints;
     this.leftLegPos = { x: 0, y: 0 };
     this.leftKneePos = { x: 0, y: 0 };
     this.leftArmPos = { x: 0, y: 0 };
     this.leftShoulderPos = { x: 0, y: 0 };
+    this.headAngle = 0;
     this.leftLegAngle = 0;
     this.leftArmAngle = 0;
     this.leftShoulderAngle = 0;
@@ -165,85 +174,95 @@ class Avatar {
   }
 
   calculateAngle() {
+    this.headAngle = Math.atan2(
+      this.joints[7].y - this.joints[8].y,
+      this.joints[7].x - this.joints[8].x
+    );
     this.shoulderAngle = Math.atan2(
-      관절[12].y - 관절[11].y,
-      관절[12].x - 관절[11].x
+      this.joints[12].y - this.joints[11].y,
+      this.joints[12].x - this.joints[11].x
     );
     this.leftArmAngle = Math.atan2(
-      관절[16].y - 관절[14].y,
-      관절[16].x - 관절[14].x
+      this.joints[16].y - this.joints[14].y,
+      this.joints[16].x - this.joints[14].x
     );
     this.leftShoulderAngle = Math.atan2(
-      관절[14].y - 관절[12].y,
-      관절[14].x - 관절[12].x
+      this.joints[14].y - this.joints[12].y,
+      this.joints[14].x - this.joints[12].x
     );
     this.rightArmAngle = Math.atan2(
-      관절[15].y - 관절[13].y,
-      관절[15].x - 관절[13].x
+      this.joints[15].y - this.joints[13].y,
+      this.joints[15].x - this.joints[13].x
     );
     this.rightShoulderAngle = Math.atan2(
-      관절[13].y - 관절[11].y,
-      관절[13].x - 관절[11].x
+      this.joints[13].y - this.joints[11].y,
+      this.joints[13].x - this.joints[11].x
     );
 
     this.leftLegAngle = Math.atan2(
-      관절[26].y - 관절[24].y,
-      관절[26].x - 관절[24].x
+      this.joints[26].y - this.joints[24].y,
+      this.joints[26].x - this.joints[24].x
     );
     this.leftKneeAngle = Math.atan2(
-      관절[28].y - 관절[26].y,
-      관절[28].x - 관절[26].x
+      this.joints[28].y - this.joints[26].y,
+      this.joints[28].x - this.joints[26].x
     );
     this.rightLegAngle = Math.atan2(
-      관절[25].y - 관절[23].y,
-      관절[25].x - 관절[23].x
+      this.joints[25].y - this.joints[23].y,
+      this.joints[25].x - this.joints[23].x
     );
     this.rightKneeAngle = Math.atan2(
-      관절[27].y - 관절[25].y,
-      관절[27].x - 관절[25].x
+      this.joints[27].y - this.joints[25].y,
+      this.joints[27].x - this.joints[25].x
     );
   }
 
-  draw(모양, pg = drawingContext) {
+  draw(head, arm, leg) {
     this.calculateAngle();
-    let upperBody = 모양.어깨너비;
-    let lowerBody = 모양.엉덩이크기;
-    let bodyHeight = 모양.몸높이;
-    let armLength = 모양.팔길이;
-    let legLength = 모양.다리길이;
-    let legWidth = 모양.다리굵기;
-    let armWidth = 모양.팔굵기;
+    // let upperBody = 모양.어깨너비;
+    // let lowerBody = 모양.엉덩이크기;
+    // let bodyHeight = 모양.몸높이;
+    let armLength = 0;
+    let legLength = 0;
+    let legWidth = 0;
+    let armWidth = 0;
 
-    let leftShoulder = { x: (관절[12].x * 0.6 + 320 * 0.4), y: 관절[12].y };
-    let rightShoulder = { x: (관절[11].x * 0.6 + 320 * 0.4), y: 관절[11].y };
-    let leftHip = { x: 관절[24].x * 0.5 + 160, y: 관절[24].y * 0.9 };
-    let rightHip = { x: 관절[23].x * 0.5 + 160, y: 관절[23].y * 0.9 };
-
-    // head
-    let d = dist(관절[7].x, 관절[7].y, 관절[8].x, 관절[8].y);
-    ellipse(관절[0].x * 0.6 + 320 * 0.4, 관절[9].y, d, d);
+    let lx = lerp(this.joints[12].x, this.joints[11].x, 1.0 - 아바타설정.몸통너비);
+    let rx = lerp(this.joints[12].x, this.joints[11].x, 아바타설정.몸통너비);    
+    let leftShoulder = { x: lx, y: this.joints[12].y };
+    let rightShoulder = { x: rx, y: this.joints[11].y };
+    lx = lerp(this.joints[24].x, this.joints[23].x, 1.0 - 아바타설정.몸통너비);
+    rx = lerp(this.joints[24].x, this.joints[23].x, 아바타설정.몸통너비);
+    let ly = lerp(this.joints[12].y, this.joints[24].y, 아바타설정.몸통길이);
+    let ry = lerp(this.joints[11].y, this.joints[23].y, 아바타설정.몸통길이);    
+    let leftHip = { x: lx, y: ly };
+    let rightHip = { x: rx, y: ry };
+    let d = dist(this.joints[12].x, this.joints[12].y, this.joints[23].x, this.joints[23].y);
+    armLength = d * 아바타설정.팔길이;
+    legLength = d * 아바타설정.다리길이;
+    armWidth = d * 아바타설정.팔굵기;
+    legWidth = d * 아바타설정.다리굵기;
 
     //body
-    stroke(255);
-    strokeWeight(legWidth * 2);
     push();
     beginShape();
-    vertex(관절[0].x * 0.6 + 320 * 0.4, 관절[9].y);
-    vertex(rightShoulder.x, rightShoulder.y);
-    vertex(rightHip.x, rightHip.y);
-    vertex(leftHip.x, leftHip.y);
-    vertex(leftShoulder.x, leftShoulder.y);
+    vertex(this.joints[0].x, this.joints[0].y);
+    vertex(rightShoulder.x + armWidth, rightShoulder.y);
+    vertex(rightHip.x + legWidth, rightHip.y);
+    vertex(leftHip.x - legWidth, leftHip.y);
+    vertex(leftShoulder.x - armWidth, leftShoulder.y);
     endShape(CLOSE);
     pop();
-    strokeWeight(0);
-    
+
     // arms
+    // left arm
+    noStroke();
     push();
     translate(leftShoulder.x, leftShoulder.y);
     push();
     translate(this.leftShoulderPos.x, this.leftShoulderPos.y);
     rotate(this.leftShoulderAngle);
-    ellipse(0, 0, 10, 10);
+    ellipse(0, 0, armWidth * 2);
     rect(0, -armWidth, armLength, armWidth * 2);
     pop();
 
@@ -253,17 +272,19 @@ class Avatar {
     push();
     translate(this.leftArmPos.x + x, this.leftArmPos.y + y);
     rotate(this.leftArmAngle);
-    ellipse(0, 0, 10, 10);
-    quad(0, -armWidth, armLength, -1, armLength, 1, 0, armWidth);
+    ellipse(0, 0, armWidth * 2);
+    quad(0, -armWidth, armLength, -2, armLength, 2, 0, armWidth);
+    arm(armLength);
     pop();
     pop();
 
+    // right arm
     push();
     translate(rightShoulder.x, rightShoulder.y);
     push();
     translate(this.rightShoulderPos.x, this.rightShoulderPos.y);
     rotate(this.rightShoulderAngle);
-    ellipse(0, 0, 10, 10);
+    ellipse(0, 0, armWidth * 2);
     rect(0, -armWidth, armLength, armWidth * 2);
     pop();
 
@@ -272,8 +293,8 @@ class Avatar {
     push();
     translate(this.rightArmPos.x + x, this.rightArmPos.y + y);
     rotate(this.rightArmAngle);
-    ellipse(0, 0, 10, 10);
-    quad(0, -armWidth, armLength, -1, armLength, 1, 0, armWidth);
+    ellipse(0, 0, armWidth * 2);
+    quad(0, -armWidth, armLength, -2, armLength, 2, 0, armWidth);
     pop();
     pop();
     
@@ -283,7 +304,7 @@ class Avatar {
     push();
     translate(this.leftLegPos.x, this.leftLegPos.y);
     rotate(this.leftLegAngle);
-    ellipse(0, 0, 10, 10);
+    ellipse(0, 0, legWidth * 2);
     rect(0, -legWidth, legLength, legWidth * 2);
     pop();
 
@@ -292,8 +313,9 @@ class Avatar {
     push();
     translate(this.leftKneePos.x + x, this.leftKneePos.y + y);
     rotate(this.leftKneeAngle);
-    ellipse(0, 0, 10, 10);
-    quad(0, -legWidth, legLength, -1, legLength, 1, 0, legWidth);
+    ellipse(0, 0, legWidth * 2);
+    quad(0, -legWidth, legLength, -2, legLength, 2, 0, legWidth);
+    leg(legLength);
     pop();
     pop();
 
@@ -302,7 +324,7 @@ class Avatar {
     push();
     translate(this.rightLegPos.x, this.rightLegPos.y);
     rotate(this.rightLegAngle);
-    ellipse(0, 0, 10, 10);
+    ellipse(0, 0, legWidth * 2);
     rect(0, -legWidth, legLength, legWidth * 2);
     pop();
 
@@ -311,10 +333,18 @@ class Avatar {
     push();
     translate(this.rightKneePos.x + x, this.rightKneePos.y + y);
     rotate(this.rightKneeAngle);
-    ellipse(0, 0, 10, 10);
-    quad(0, -legWidth, legLength, -1, legLength, 1, 0, legWidth);
+    ellipse(0, 0, legWidth * 2);
+    quad(0, -legWidth, legLength, -2, legLength, 2, 0, legWidth);
     pop();
     pop();
-  }
 
+    // head
+    // d = dist(관절[7].x, 관절[7].y, 관절[8].x, 관절[8].y);
+    push();
+    translate(this.joints[0].x, this.joints[0].y);
+    rotate(this.headAngle);
+    // ellipse(0, 0, d * 아바타설정.머리크기);
+    head(d * 아바타설정.머리크기);
+    pop(); 
+  }
 }
